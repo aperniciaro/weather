1d733afaf4c51b5192be8e9a4d8a44adbc25888b
Andrew Perniciaro <andrew.perniciaro@gmail.com>
2019-02-12 15:38:36 -0500

    used isNaN

--- public/main.js
+++ public/main.js
@@ -1,40 +1,44 @@
-
-// weather search function
-//   input location
-//   fetch weather api
-//   then JSON
-//   store json obj in variable
-
-// output temperature
-// output other weather info
-
-// event listener for button click, send to location function
 let currentReport = {}
 
-const weather = () => {
+const weatherReport = () => {
   const location = document.querySelector('.location-field').value
-  if(Number(location) == NaN){
-    fetch('api.openweathermap.org/data/2.5/weather?q={'+location+'}&appid=27c656c95af0cc6ac0d65538f53aab04')
+  const isCityOrZip = isNaN(location)
+  if (isCityOrZip == true) {
+    const cityUrl =
+      'https://api.openweathermap.org/data/2.5/weather?q=' +
+      location +
+      '&appid=27c656c95af0cc6ac0d65538f53aab04&units=imperial'
+    fetch(cityUrl)
       .then(resp => {
         return resp.json()
       })
-      then(report => {
+      .then(report => {
         currentReport = report
+        console.log(currentReport)
       })
-  }else{
-    fetch('api.openweathermap.org/data/2.5/weather?zip={'+location+'}&appid=27c656c95af0cc6ac0d65538f53aab04')
-    .then(resp => {
-      return resp.json()
-    })
-    then(report => {
-      currentReport = report
-    })
+  } else {
+    const zipUrl =
+      'https://api.openweathermap.org/data/2.5/weather?zip=' +
+      location +
+      '&appid=27c656c95af0cc6ac0d65538f53aab04&units=imperial'
+    fetch(zipUrl)
+      .then(resp => {
+        return resp.json()
+      })
+      .then(report => {
+        currentReport = report
+        console.log(currentReport)
+      })
   }
 
-  const temp = document.createElement('h3')
-  temp.textContent = currentReport.main.temp
+  const temperature = document.createElement('h3')
+  temperature.textContent = currentReport.main.temp
+  document.querySelector('body').appendChild(temperature)
   const conditions = document.createElement('h3')
   conditions.textContent = currentReport.weather.description
+  document.querySelector('body').appendChild(conditions)
 }
 
-document.querySelector('.search-button).addEventListener('click', weather)
+document
+  .querySelector('.search-button')
+  .addEventListener('click', weatherReport)
