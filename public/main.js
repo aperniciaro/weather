428a3fdbc9b06ac74632c3216f122fdd780414f4
Andrew Perniciaro <andrew.perniciaro@gmail.com>
2019-02-12 14:32:17 -0500

    created weather function

--- public/main.js
+++ public/main.js
@@ -1,13 +1,6 @@
-// location function
-//   input from text box
-//   evaluate if input is city or zip
-//   fetch location api
-//   then json
-//   store long and lat in variables
-//   call weather function
 
 // weather search function
-//   input long and lat
+//   input location
 //   fetch weather api
 //   then JSON
 //   store json obj in variable
@@ -16,3 +9,32 @@
 // output other weather info
 
 // event listener for button click, send to location function
+let currentReport = {}
+
+const weather = () => {
+  const location = document.querySelector('.location-field').value
+  if(Number(location) == NaN){
+    fetch('api.openweathermap.org/data/2.5/weather?q={'+location+'}&appid=27c656c95af0cc6ac0d65538f53aab04')
+      .then(resp => {
+        return resp.json()
+      })
+      then(report => {
+        currentReport = report
+      })
+  }else{
+    fetch('api.openweathermap.org/data/2.5/weather?zip={'+location+'}&appid=27c656c95af0cc6ac0d65538f53aab04')
+    .then(resp => {
+      return resp.json()
+    })
+    then(report => {
+      currentReport = report
+    })
+  }
+
+  const temp = document.createElement('h3')
+  temp.textContent = currentReport.main.temp
+  const conditions = document.createElement('h3')
+  conditions.textContent = currentReport.weather.description
+}
+
+document.querySelector('.search-button).addEventListener('click', weather)
