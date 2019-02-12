e6489f785065aafd4c8f9eac8bbc41658878c564
Andrew Perniciaro <andrew.perniciaro@gmail.com>
2019-02-12 13:28:20 -0500

    html structure and js outline

--- public/main.js
+++ public/main.js
@@ -1,7 +1,18 @@
-const main = () => {
-  if (document.querySelector('h1.hello-world')) {
-    document.querySelector('h1.hello-world').textContent = 'Hello, World!'
-  }
-}
+// location function
+//   input from text box
+//   evaluate if input is city or zip
+//   fetch location api
+//   then json
+//   store long and lat in variables
+//   call weather function
 
-document.addEventListener('DOMContentLoaded', main)
+// weather search function
+//   input long and lat
+//   fetch weather api
+//   then JSON
+//   store json obj in variable
+
+// output temperature
+// output other weather info
+
+// event listener for button click, send to location function
